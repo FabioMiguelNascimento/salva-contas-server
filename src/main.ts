82b9431025627f7909bc21ef3fe2278f2030cb4a
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2025-12-04 22:18:28 -0300

    feat: enable CORS with specified methods and options in main application

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
 
   app.useGlobalFilters(new GlobalExceptionHandler());
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
